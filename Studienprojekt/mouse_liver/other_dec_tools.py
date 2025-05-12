ed6a5139a95abff8d333acad7cc99daf9c129785
ItsReallyMe2 <golubovicswork5@gmail.com>
2025-05-12 14:30:35 +0200

    Finished linear and non-linear dec tools

--- Studienprojekt/mouse_liver/other_dec_tools.py
+++ Studienprojekt/mouse_liver/other_dec_tools.py
@@ -9,14 +9,20 @@ from sklearn.decomposition import TruncatedSVD
 from sklearn.decomposition import FactorAnalysis
 from sklearn.decomposition import FastICA
 from sklearn.decomposition import NMF
+from sklearn.manifold import TSNE
+import umap as um
+
 '''Open DataSets'''
 path_1 = '/Users/maksimsgolubovics/Python_VScode/Studienprojekt' #add your path to the project
 path = path_1+'/mouse_liver_noGH/mice.h5ad'
 adata = ad.read_h5ad(path)
-#cs_df = adata.obsm['centered_study'].join(adata.obs['time']).reset_index().drop(columns='index').set_index('time').sort_index(ascending=True)
+cs_df = adata.obsm['centered_study'].join(adata.obs['time']).reset_index().drop(columns='index').set_index('time').sort_index(ascending=True)
+# cs_df_pos = cs_df + 6
 '''Dataset for NMF'''
-cs_log1_df = adata.obsm['centered_study_log+1'].join(adata.obs['time']).reset_index().drop(columns='index').set_index('time').sort_index(ascending=True)
-cs_log1_df_pos = cs_log1_df+5
+# cs_log1_df = adata.obsm['centered_study_log+1'].join(adata.obs['time']).reset_index().drop(columns='index').set_index('time').sort_index(ascending=True)
+# cs_log1_df_pos = cs_log1_df+5
+
+'''Linear Decompositional tools'''
 '''PCA as baseline'''
 # pc.principal_component_3d_timesample(data=cs_log1_df_pos, label='Time in h')
 '''PCA with randomized SVD'''
@@ -27,10 +33,23 @@ cs_log1_df_pos = cs_log1_df+5
 # pc.visualization_of_dec_tools_3d(dec=FactorAnalysis(n_components=3), data=cs_df, label='Time in h', c_map='twilight')
 # pc.visualization_of_dec_tools_3d(dec=FactorAnalysis(n_components=3, rotation='varimax'), data=cs_df, label='Time in h', c_map='twilight')
 # pc.visualization_of_dec_tools_3d(dec=FactorAnalysis(n_components=3, rotation='quartimax'), data=cs_df, label='Time in h', c_map='twilight')
+'''NMF with log10(x+1)5'''
+# pc.visualization_of_dec_tools_3d(dec=NMF(n_components=3), data=cs_log1_df_pos, label='Time in h', c_map='twilight')
+# pc.visualization_of_dec_tools_3d(dec=NMF(n_components=3, solver='mu'), data=cs_log1_df_pos, label='Time in h', c_map='twilight')
+# pc.visualization_of_dec_tools_3d(dec=NMF(n_components=3, solver='mu', beta_loss='itakura-saito'), data=cs_log1_df_pos, label='Time in h', c_map='twilight')
+# pc.visualization_of_dec_tools_3d(dec=NMF(n_components=3, solver='mu', beta_loss='kullback-leibler'), data=cs_log1_df_pos, label='Time in h', c_map='twilight')
+'''NMF with log10(x+0.1)+6'''
+# pc.visualization_of_dec_tools_3d(dec=NMF(n_components=3), data=cs_df_pos, label='Time in h', c_map='twilight')
+# pc.visualization_of_dec_tools_3d(dec=NMF(n_components=3, solver='mu'), data=cs_df_pos, label='Time in h', c_map='twilight')
+# pc.visualization_of_dec_tools_3d(dec=NMF(n_components=3, solver='mu', beta_loss='itakura-saito'), data=cs_df_pos, label='Time in h', c_map='twilight')
+# pc.visualization_of_dec_tools_3d(dec=NMF(n_components=3, solver='mu', beta_loss='kullback-leibler'), data=cs_df_pos, label='Time in h', c_map='twilight')
+
+
+'''Non Linear Decompositional tools'''
+'''t-SNE'''
+# pc.visualization_of_dec_tools_3d(dec=TSNE(n_components=3, random_state= 32), data=cs_df, label='Time in h', c_map='twilight')
 '''FastICA'''
 # pc.visualization_of_dec_tools_3d(dec=FastICA(n_components=3), data=cs_df, label='Time in h')
 # pc.visualization_of_dec_tools_3d(dec=FastICA(n_components=3), data=cs_df, label='Time in h', c_map='twilight')
-'''NMF'''
-# pc.visualization_of_dec_tools_3d(dec=NMF(n_components=3), data=cs_log1_df_pos, label='Time in h', c_map='twilight')
-# pc.visualization_of_dec_tools_3d(dec=NMF(n_components=3, solver='mu', beta_loss='itakura-saito'), data=cs_log1_df_pos, label='Time in h', c_map='twilight')
-# pc.visualization_of_dec_tools_3d(dec=NMF(n_components=3, solver='mu', beta_loss='kullback-leibler'), data=cs_log1_df_pos, label='Time in h', c_map='twilight')
+'''UMAP'''
+# pc.visualization_of_dec_tools_3d(dec=um.UMAP(n_components=3), data=cs_df, label='Time in h', c_map='twilight')
